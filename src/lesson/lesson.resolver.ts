4b61a3213b4ef09c034e5eaffb440320159968c6
Thongchai Sidthikhattkorn - THA <ThongchS@th.fwd.com>
2022-01-24 11:01:00 +0700

    add get all lessons

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,24 +1,29 @@
 import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { LessonType } from './lesson.type';
 import { LessonService } from './lesson.service';
+import { CreateLessonInput } from './lesson.input';
 
 @Resolver((of) => LessonType)
 export class LessonResolver {
   constructor(private lessonService: LessonService) {}
 
   @Query((returns) => LessonType)
-  lesson(
-    @Args('id')id:string
-  ) {
+  lesson(@Args('id') id: string) {
     return this.lessonService.getLesson(id);
+  }
+
+  @Query((returns) => [LessonType])
+  lessons() {
+    return this.lessonService.getLessons();
   }
 
   @Mutation((returns) => LessonType)
   createLesson(
-    @Args('name') name: string,
-    @Args('startDate') startDate: string,
-    @Args('endDate') endDate: string,
+    @Args('createLessonInput') createLessonInput: CreateLessonInput,
+    // @Args('name') name: string,
+    // @Args('startDate') startDate: string,
+    // @Args('endDate') endDate: string,
   ) {
-    return this.lessonService.createLesson(name, startDate, endDate);
+    return this.lessonService.createLesson(createLessonInput);
   }
 }
